deca8d0e4c7fd813ea78b954ab21cdcc24be35ff
jhatcher <jhatcher@joshs-mbp.memphis.edu>
2017-02-12 19:32:39 -0600

    Added new abstract classes.

--- Iteration0Skeleton/src/payment/Project.java
+++ Iteration0Skeleton/src/payment/Project.java
@@ -1,5 +1,12 @@
 package payment;
 
 public abstract class Project {
-
+	int projectID; //ID for the project, should auto increment in DB
+	String projectName; //Name for the project
+	int vendorID; // ID for vendor
+	Double payRate; //payrate for whomever accepts this project
+	
+	public Project(String inProjectName, int inVendorID, double inPayRate){
+		
+	}
 }
