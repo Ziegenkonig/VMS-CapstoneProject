d3986f5b4ad6fb993eca32ade933c17ba71f915b
kbridson <kbridson@memphis.edu>
2017-02-23 00:51:31 -0600

    Adding the spring framework we made at today's mtg
    I also added the beginnings of the Invoice entity as an example of JPA.
    I had to add a special gitignore file because the metadata folder is
    enormous and i don't think its necessary to share it. You should get
    your own copy of all the other folders when you treat this folder as
    workspace in STS.

---
+++ hp-tech-systems-vms/src/main/java/com/vms/models/Invoice.java
@@ -0,0 +1,38 @@
+package com.vms.models;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Invoice {
+
+	@Id @GeneratedValue()
+	private int invoice_id;
+	
+	@Enumerated(EnumType.STRING)
+	private InvoiceStatus status;
+	
+	private Date period_start;
+	private Date period_end;
+	private Date payment_due;
+	private String recruiter;
+	
+	private double total_hours;
+	private BigDecimal total_amt;
+	
+	//should this be here or FK to project
+	private BigDecimal rate;
+	
+	
+	
+	
+}
