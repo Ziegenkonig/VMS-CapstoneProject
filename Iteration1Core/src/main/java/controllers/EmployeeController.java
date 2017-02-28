6c13e70b6a1d404bb37603684ffdb69d2c6c36c9
Josh Hatcher <jbhtcher@memphis.edu>
2017-02-27 21:14:58 -0600

    Got employee controller running.

--- Iteration1Core/src/main/java/controllers/EmployeeController.java
+++ Iteration1Core/src/main/java/controllers/EmployeeController.java
@@ -1,1 +1,15 @@
 package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EmployeeController{
+  @RequestMapping("/sayEmployeeName")
+  public String sayEmployeeName(@RequestParam(value = "name", required = false, defaultValue = "NO NAME SUPPLIED")String name, Model model) {
+    model.addAttribute("name", name);
+    return "sayEmployeeName";
+  }
+}
