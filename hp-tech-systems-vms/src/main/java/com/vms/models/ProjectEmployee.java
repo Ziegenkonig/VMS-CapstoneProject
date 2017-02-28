28bb4f693c87d9f056760fa66ec9abca6745ca9b
Alex <Ziegenkonigreality@yahoo.com>
2017-02-28 14:04:26 -0600

    Created EmployeeProject entity, added necessary relations to Employee and Project Entities

---
+++ hp-tech-systems-vms/src/main/java/com/vms/models/ProjectEmployee.java
@@ -0,0 +1,5 @@
+package com.vms.models;
+
+public class ProjectEmployee {
+
+}
--- hp-tech-systems-vms/src/main/java/com/vms/models/Timesheet.java
+++ hp-tech-systems-vms/src/main/java/com/vms/models/Timesheet.java
@@ -1,5 +1,18 @@
 package com.vms.models;
 
-public class Timesheet {
+import java.math.BigDecimal;
+import java.sql.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Timesheet {
+	
 }
