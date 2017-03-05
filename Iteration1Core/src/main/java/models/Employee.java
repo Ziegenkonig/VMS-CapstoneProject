96149769c48617f36a19b20210313f15cf5bc9b6
Josh Hatcher <jbhtcher@memphis.edu>
2017-03-04 18:10:25 -0600

    Added getters and setters.

--- Iteration1Core/src/main/java/models/Employee.java
+++ Iteration1Core/src/main/java/models/Employee.java
@@ -1,21 +1,91 @@
 package models;
 
 public class Employee{
-  private long id;
-  private String content;
+  int id;
+  int permissionLevel;
+  String username;
+  String password;
+  String firstName;
+  String lastName;
+  String email;
+  String address;
+  String city;
+  String state;
+  String hireDate;
+  boolean isActive;
 
-  public long getId(){
+
+
+/**
+Getters and Setters
+                   **/
+
+          //getters
+  public int getId(){
     return id;
   }
-
-  public void setId(long id){
+  public int getPermissionLevel(){
+    return permissionLevel;
+  }
+  public String getUsername(){
+    return username;
+  }
+  public String getPassword(){
+    return password;
+  }
+  public String getFirstName(){
+    return firstName;
+  }
+  public String getLastName(){
+    return lastName;
+  }
+  public String getEmail(){
+    return email;
+  }
+  public String getAddress(){
+    return address;
+  }
+  public String getState(){
+    return state;
+  }
+  public String getHireDate(){
+    return hireDate;
+  }
+  public String getIsActive(){
+    return isActive;
+  }
+        //setters
+  public void setId(int id){
     this.id = id;
   }
-
-  public String getContent() {
-    return content;
+  public int setPermissionLevel(int permissionLevel){
+    this.permissionLevel = permissionLevel;
   }
-  public void setContent(String content){
-    this.content = content;
+  public String setUsername(){
+    this.username = username;
+  }
+  public String setPassword(){
+    this.password = password;
+  }
+  public String setFirstName(){
+    this.firstName = firstName ;
+  }
+  public String setLastName(){
+    this.lastName = lastName;
+  }
+  public String setEmail(){
+    this.email = email;
+  }
+  public String setAddress(){
+    this.address = address;
+  }
+  public String setState(){
+    this.state = state;
+  }
+  public String setHireDate(){
+    this.hireDate = hireDate;
+  }
+  public String setIsActive(){
+    this.isActive = isActive;
   }
 }
