87ebf87197f6bb48c95e142da0323a7a8ea2492b
Josh Hatcher <jbhtcher@memphis.edu>
2017-03-05 17:25:40 -0600

    The employee view and controller are working pretty well together.

--- Iteration1Core/src/main/java/controllers/EmployeeController.java
+++ Iteration1Core/src/main/java/controllers/EmployeeController.java
@@ -1,14 +1,19 @@
 package controllers;
-
+//Spring imports
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import models.Employee;
+//arraylist imports
+import java.io.IOException;
+import java.util.ArrayList;
 
 @Controller
 public class EmployeeController{
+  //declare global variables
+  ArrayList<Employee> employees = new ArrayList<Employee>();
 
   @GetMapping("/employee")
   public String employeeForm(Model model){
@@ -27,7 +32,10 @@ public class EmployeeController{
 
   @PostMapping("/employee")
   public String employeeSubmit(@ModelAttribute Employee employee){
+    //This is where the pipe of data ends. Anything that needs to be persisted needs to happen here.
     System.out.println("" + employee.getId());
+    System.out.println("" + employee.getUsername());
+    System.out.println("" + employee.getPassword());
     return "result";
   }
 }
--- Iteration1Core/src/main/java/models/Employee.java
+++ Iteration1Core/src/main/java/models/Employee.java
@@ -12,7 +12,7 @@ public class Employee{
   String city;
   String state;
   String hireDate;
-  boolean isActive;
+  //boolean isActive;
 
 
 
@@ -45,47 +45,53 @@ Getters and Setters
   public String getAddress(){
     return address;
   }
+  public String getCity(){
+    return city;
+  }
   public String getState(){
     return state;
   }
   public String getHireDate(){
     return hireDate;
   }
-  public String getIsActive(){
-    return isActive;
-  }
+  //public boolean getIsActive(){
+    //return isActive;
+  //}
         //setters
   public void setId(int id){
     this.id = id;
   }
-  public int setPermissionLevel(int permissionLevel){
+  public void setPermissionLevel(int permissionLevel){
     this.permissionLevel = permissionLevel;
   }
-  public String setUsername(){
+  public void setUsername(String username){
     this.username = username;
   }
-  public String setPassword(){
+  public void setPassword(String password){
     this.password = password;
   }
-  public String setFirstName(){
+  public void setFirstName(String firstName){
     this.firstName = firstName ;
   }
-  public String setLastName(){
+  public void setLastName(String lastName){
     this.lastName = lastName;
   }
-  public String setEmail(){
+  public void setEmail(String email){
     this.email = email;
   }
-  public String setAddress(){
+  public void setAddress(String address){
     this.address = address;
   }
-  public String setState(){
+  public void setCity(String city){
+    this.city = city;
+  }
+  public void setState(String state){
     this.state = state;
   }
-  public String setHireDate(){
+  public void setHireDate(String hireDate){
     this.hireDate = hireDate;
   }
-  public String setIsActive(){
-    this.isActive = isActive;
-  }
+  //public void setIsActive(){
+    //this.isActive = isActive;
+  //}
 }
