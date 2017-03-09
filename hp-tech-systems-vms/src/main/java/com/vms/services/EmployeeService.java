d0dd067cee270f6ece557209667bb3b3f2347a7d
Alex Ziegenhorn <Alex Ziegenhorn>
2017-03-09 14:36:17 -0600

    added basice Employee Service methods

--- hp-tech-systems-vms/src/main/java/com/vms/services/EmployeeService.java
+++ hp-tech-systems-vms/src/main/java/com/vms/services/EmployeeService.java
@@ -1,10 +1,14 @@
 package com.vms.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vms.models.Employee;
+import com.vms.models.ProjectEmployee;
 import com.vms.repositories.EmployeeRepository;
+import com.vms.repositories.ProjectEmployeeRepository;
 
 /*
  * Method to check whether an employee with the given username and password exists (login/register)
@@ -18,6 +22,30 @@ public class EmployeeService {
 	//Pulling in the repository
 	@Autowired
 	EmployeeRepository employeeRepo;
+
+	//General methods
+	
+	//creates a new object in table
+	void create(Employee employee) {
+		employeeRepo.save(employee);
+	}
+	
+	//update a new object in table
+	void update(Employee employee) {
+		employeeRepo.save(employee);
+	}
+	
+	//Search methods
+	
+	//returns one object with specified id
+	Employee findOne(Integer id) {
+		return employeeRepo.findOne(id);
+	}
+	
+	//returns all objects
+	List<Employee> findAll() {
+		return employeeRepo.findAll();
+	}
 	
 	//login() checks to see if an employee exists in the database with the given username
 	//if an employee is found, the password is checked against the given password
