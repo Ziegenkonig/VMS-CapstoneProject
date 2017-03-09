9d1339cf5ba9b0864d3941e2c3e384b4dc94583e
Josh Hatcher <jbhtcher@memphis.edu>
2017-03-08 21:05:09 -0600

    Created invoice form.

--- hp-tech-systems-vms/src/main/java/com/vms/models/Invoice.java
+++ hp-tech-systems-vms/src/main/java/com/vms/models/Invoice.java
@@ -17,7 +17,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import lombok.Data; 
+import lombok.Data;
 
 /* can I get the following with some get methods?
 a single vendor object (timesheet.proj-emp.project.vendor)
@@ -32,43 +32,43 @@ public class Invoice {
 
 	@Id @GeneratedValue
 	private int invoice_id;
-	
+
 	@Enumerated(EnumType.STRING)
 	private InvoiceStatus status;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
     @Column(updatable = false)
     private Date created_date;
-	
+
 //fields to store info from other tables
 	//from project
 	private BigDecimal rate; //timesheet.projemp.project.pay_rate
-	
+
 	private double total_hours; //sum(timesheet.no_hours) over timesheet list
 	private BigDecimal total_amt; //total_hours * rate
-	
+
 	//pay period start and end (14/7 day interval)
 	private Date period_start;
 	private Date period_end;
 
 	// one month from period_end
 	private Date payment_due;
-	
+
 	//from vendor.contact_name
 	private String recruiter;
-	
+
 	//from vendor
 	private String name;
 	private String address;
 	private String city;
 	private String state;
 	private String phone;
-	
+
 	//employer info saved in global variables somewhere
-	
+
 	@ManyToMany(mappedBy = "invoices")//(cascade = CascadeType.ALL)
-    private List<Timesheet> timesheets; 
-	
+    private List<Timesheet> timesheets;
+
 	@PrePersist
 	protected void onCreate() {
 		created_date = new Date(Calendar.getInstance().getTime().getTime());
