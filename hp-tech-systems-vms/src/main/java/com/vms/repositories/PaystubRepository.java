44ed506734896ddfa4611aeada6e52f1addb0abd
kbridson <kbridson@memphis.edu>
2017-03-10 00:17:28 -0600

    Query changes

--- hp-tech-systems-vms/src/main/java/com/vms/repositories/PaystubRepository.java
+++ hp-tech-systems-vms/src/main/java/com/vms/repositories/PaystubRepository.java
@@ -3,19 +3,14 @@ package com.vms.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.vms.models.Paystub;
 
 public interface PaystubRepository extends JpaRepository<Paystub, Integer>{
 	
+	//find all paystubs by employee
+	@Query
 	public List<Paystub> findByEmpIdOrderByPeriodStartDesc(int emp_id);
-	/*
-	public List<Timesheet> getCurrentTimesheets(Employee e) {
-		List timesheets = new ArrayList<Timesheet>();
-		for(ProjectEmployee pe: e.getProjemps())
-			pe.getTimesheets();
-		e.getProjemps();
-		return 
-	}
-	*/
+	
 }
