016a4b74381287d4285372e990802e8eb8ffa0f6
Josh Hatcher <jbhtcher@memphis.edu>
2017-03-22 19:36:25 -0500

    Deleting team's structure so I can work with gradle. long story short: Maven = :(

--- hp-tech-systems-vms/src/main/java/com/vms/models/InvoiceStatus.java
+++
@@ -1,9 +0,0 @@
-package com.vms.models;
-
-public enum InvoiceStatus {
-	DRAFT,
-	PENDING,
-	PAID,
-	ARCHIVED,
-	VOIDED
-}
--- hp-tech-systems-vms/src/main/java/com/vms/models/TimesheetStatus.java
+++
@@ -1,8 +0,0 @@
-package com.vms.models;
-
-public enum TimesheetStatus {
-	NOT_SUBMITTED,
-	PENDING,
-	VERIFIED,
-	ARCHIVED	
-}
