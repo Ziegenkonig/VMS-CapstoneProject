8fd50064ddebe2bd8418fe24ab23c7a8f2ac06b4
Josh Hatcher <jbhtcher@memphis.edu>
2017-03-29 13:57:29 -0500

    Mail works upon application startup.

--- hp-tech-systems-vms/src/main/java/com/vms/HpTechSystemsVmsApplication.java
+++ hp-tech-systems-vms/src/main/java/com/vms/HpTechSystemsVmsApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.vms.utilities.Mail;
+
 @EnableJpaRepositories("com.vms.repositories")
 @SpringBootApplication
 public class HpTechSystemsVmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HpTechSystemsVmsApplication.class, args);
+		Mail.sendEmail();
 	}
 }
