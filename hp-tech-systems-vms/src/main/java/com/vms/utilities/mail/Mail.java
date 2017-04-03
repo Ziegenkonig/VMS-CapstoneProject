c4f12798fffd6c12ff0274d64217363e6de8a5a6
Josh Hatcher <jbhtcher@memphis.edu>
2017-04-02 19:21:05 -0500

    Created a MailSender class to hold all of the sender properties.

--- hp-tech-systems-vms/src/main/java/com/vms/utilities/mail/Mail.java
+++ hp-tech-systems-vms/src/main/java/com/vms/utilities/mail/Mail.java
@@ -11,29 +11,15 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
 import com.vms.models.Employee;
 
 public class Mail {
 	public static void sendEmail() {
-		final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
-		// Get a Properties object
-		Properties props = System.getProperties();
-		props.setProperty("mail.smtp.host", "smtp.gmail.com");
-		props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
-		props.setProperty("mail.smtp.socketFactory.fallback", "false");
-		props.setProperty("mail.smtp.port", "465");
-		props.setProperty("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.debug", "true");
-		props.put("mail.store.protocol", "pop3");
-		props.put("mail.transport.protocol", "smtp");
-		final String username = "uofmcapstonebanana@gmail.com";//
-		final String password = "alwaysmoneyinthebananastand123";
+		MailSender sender = new MailSender();
 		try {
-			Session session = Session.getInstance(props, new Authenticator() {
+			Session session = Session.getInstance(sender.getProps(), new Authenticator() {
 				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(username, password);
+					return new PasswordAuthentication(sender.getUsername(), sender.getPassword());
 				}
 			});
 
@@ -58,21 +44,5 @@ public class Mail {
 			System.out.println("Erreur d'envoi, cause: " + e);
 		}
 	}
-	private static String generateTextMessage(Employee employee){
-		//get an employee as input to the method, along with some othe rkind of necessary details, then 
-		//convert it into a string that can be sent as a text message in the email. 
-		String greeting = "Hello " + employee.getFirstname() + ", this is your name!\n";
-		
-		// -- Want to generate a URL that links to the invoices left to fill out, or the invoices that need to be viewed or whatever --
-		//String url = "";
-		
-		return "" + greeting;
-	}
-	private String timesheetSubmittedNotification(Employee employee){
-		//employee has submitted their timesheet, so the employer needs to know
-		String output = "" + employee.getFirstname() + " " + employee.getLastname() + " has submitted a timesheet";
-		return output;
-	}
-
 	}
 
---
+++ hp-tech-systems-vms/src/main/java/com/vms/utilities/mail/MailSender.java
@@ -0,0 +1,64 @@
+package com.vms.utilities.mail;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import com.vms.models.Employee;
+
+public class MailSender {
+	String username;
+	String password;
+	Properties props;
+	
+
+	public MailSender() {
+		final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+		// Get a Properties object
+		Properties props = System.getProperties();
+		props.setProperty("mail.smtp.host", "smtp.gmail.com");
+		props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+		props.setProperty("mail.smtp.socketFactory.fallback", "false");
+		props.setProperty("mail.smtp.port", "465");
+		props.setProperty("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.debug", "true");
+		props.put("mail.store.protocol", "pop3");
+		props.put("mail.transport.protocol", "smtp");
+		final String username = "uofmcapstonebanana@gmail.com";//
+		final String password = "alwaysmoneyinthebananastand123";
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Properties getProps() {
+		return props;
+	}
+
+	public void setProps(Properties props) {
+		this.props = props;
+	}
+	
+}
