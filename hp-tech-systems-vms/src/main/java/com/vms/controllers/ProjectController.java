5d845e200ef24239b0a33dcd56e98efe054a33a9
Alex Ziegenhorn <Alex Ziegenhorn>
2017-04-06 21:59:20 -0500

    Setup input validation for new project form

--- hp-tech-systems-vms/src/main/java/com/vms/controllers/ProjectController.java
+++ hp-tech-systems-vms/src/main/java/com/vms/controllers/ProjectController.java
@@ -2,46 +2,92 @@ package com.vms.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.vms.models.Project;
+import com.vms.models.Vendor;
+import com.vms.services.EmployeeService;
+import com.vms.services.ProjectEmployeeService;
 import com.vms.services.ProjectService;
+import com.vms.services.VendorService;
 
 @Controller
 public class ProjectController {
 
 	@Autowired
-	private ProjectService projectService = new ProjectService();
-
+	private ProjectService pService = new ProjectService();
+	@Autowired
+	private VendorService vService = new VendorService();
+	
+	@Autowired
+	private EmployeeService eService = new EmployeeService();
+	
+	@Autowired
+	private ProjectEmployeeService peService = new ProjectEmployeeService();
+	
+	
 	@GetMapping(value = "/projects")
 	public String viewProjects(Model model) {
-		List<Project> projects = projectService.findAll();
+		List<Project> projects = pService.findAll();
 		model.addAttribute("projects", projects);
 		return "project/projects";
 	}
 	
 	@GetMapping(value = "/project/view/{name}")
 	public String viewProject(@PathVariable String name, Model model) {
-		model.addAttribute("project", projectService.findByName(name));
+		model.addAttribute("project", pService.findByName(name));
 		return "project/viewP";
 	}
 	
 	@GetMapping(value = "/project/new")
-	public String newProject(Model model) {
-		Project v = new Project();
-		model.addAttribute("project", v);
+	public String newProject(@RequestParam(required = false) Integer vId, 
+							 Model model) {
+		Project p = null;
+		if(vId == null) {
+			p = new Project();
+		} else {
+			Vendor v = vService.findOne(vId);
+			p = new Project(v);
+		}
+		List<Vendor> vendors = vService.findAll();
+		model.addAttribute("vendors", vendors);
+		model.addAttribute("project", p);
 		return "project/newP";
 	}
+	
+	@PostMapping("/project/new")
+	public String createProject(@RequestParam(value = "vendor", required = false) Vendor v, 
+								@ModelAttribute("project")@Valid Project p, 
+								BindingResult bindingResult,
+								SessionStatus status) {
+		//Checks to see if the input has any errors and renders the page over again with the errors included if it does
+		if (bindingResult.hasErrors())
+			return "project/newP";
+		
+		if(p.getVendor() == null) {
+			p.setVendor(v);
+		}
+		pService.create(p);
+		status.setComplete();
+		return "redirect:/projects";
+}
 	
 	@GetMapping(value = "/project/edit/{name}")
 	public String editProject(
 			@PathVariable String name, 
 			Model model) {
-		model.addAttribute("project", projectService.findByName(name));
+		model.addAttribute("project", pService.findByName(name));
 		return "project/editP";
 	}
   
