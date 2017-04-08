5987857e153e6aaae7715f6730a730bfac50717a
Alex Ziegenhorn <Alex Ziegenhorn>
2017-04-07 19:10:03 -0500

    implemented regeneration of existing invoices with status of DRAFT

--- hp-tech-systems-vms/src/main/java/com/vms/controllers/InvoiceController.java
+++ hp-tech-systems-vms/src/main/java/com/vms/controllers/InvoiceController.java
@@ -1,21 +1,21 @@
 package com.vms.controllers;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.vms.models.Invoice;
+import com.vms.models.InvoiceStatus;
 import com.vms.models.Project;
 import com.vms.models.ProjectTimesheet;
 import com.vms.models.Vendor;
@@ -41,41 +41,37 @@ public class InvoiceController {
 	@GetMapping("/invoice/new")
 	public String invoiceForm(Model model) {
 		
-		//Need a string to keep track of selected date since Thymeleaf and LocalDate dont play well together
-		//I had to create a new object that specifically holds a string as an attribute to pass it correctly
-		StringHolder stringHolder = new StringHolder();
 		//Here we have to transfer all of the LocalDates inside of uniqueDates into Strings because LocalDates are dumb
 		//Or maybe I am and I just dont know what im doing
 		List<LocalDate> uniqueDates = projTimeService.uniqueDates();
-		List<String> allDates = new ArrayList<String>();
-		for (LocalDate date : uniqueDates) 
-			allDates.add(date.toString());
-		
+
 		//Setting all of the model attributes that are used in the corresponding html
-		model.addAttribute("stringHolder", stringHolder); //contains the user selected date in string format
-		model.addAttribute("allDates", allDates); //All unique pay periods
-		model.addAttribute("selectedVendor", new Vendor()); //Pass this to @PostMapping
-		model.addAttribute("vendors", vendorService.findAllNames()); //All Vendors
+		model.addAttribute("allDates", uniqueDates); //All unique pay periods
+		model.addAttribute("vendorNames", vendorService.findAllNames()); //All Vendors
 		
 		return "invoice/newI";
 	}
 
 	//Everything in here happens after the user presses the submit button, and is executes in-between pages
 	@PostMapping("/invoice/new")
-	public String invoiceSubmit(@ModelAttribute("selectedVendor") Vendor selectedVendor,
-								@ModelAttribute("stringHolder") StringHolder stringHolder) {
+	public String invoiceSubmit(@ModelAttribute("vendorName") String vendorName,
+			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam("date") LocalDate date) {
 		
+		System.out.println(vendorName);
+		System.out.println(date.toString());
+		System.out.println(date.getClass());
+
 		//Grabbing vendor object associated with the name selected by user
-		selectedVendor = vendorService.findByName(selectedVendor.getName());
+		Vendor v = vendorService.findByName(vendorName);
 		//Putting all projects associated with the vendor into a list
-		List<Project> projects = projectService.findByVendor(selectedVendor);
+		List<Project> projects = projectService.findByVendor(v);
 
 		//Cycling through projects list and appending all of the associated ProjectTimesheets into one list
 		//(within pay period)
 		List<ProjectTimesheet> projectTimesheets = new ArrayList<ProjectTimesheet>();
 		for (Project project : projects)
-			projectTimesheets.addAll(projTimeService.timesheetsForInvoice(project, LocalDate.parse(stringHolder.string)));
-		
+			projectTimesheets.addAll(projTimeService.timesheetsForInvoice(project.getProjectId(), date));
+
 		//Creating new invoice
 		Invoice newInvoice = new Invoice(projectTimesheets);
 		invoiceService.create(newInvoice);
@@ -86,17 +82,42 @@ public class InvoiceController {
 	
 	//VIEWING ALL INVOICES
 	//Needs countermeasures to make sure only authorized people can view
-	@GetMapping("/invoices")
+	@GetMapping(value = "/invoices")
 	public String viewInvoices(Model model) {
 		
 		//Getting all invoices
 		List<Invoice> invoices = invoiceService.findAll();
-
-		//Adding all invoices to the model
-		model.addAttribute("invoices", invoices);
 		
+		//Adding all invoices to the model
+		model.addAttribute("draft", InvoiceStatus.DRAFT);
+		model.addAttribute("invoices", invoices);
+		model.addAttribute("projectService", projectService);
 		//specifying which html file rendering
 		return "invoice/invoices";
+	}
+	
+	@GetMapping("invoice/regenerate/{id}")
+	public String regenerateInvoice(@PathVariable("id") Integer invoiceId) {
+		
+		Invoice regenInvoice = invoiceService.findById(invoiceId);
+		
+		//Grabbing vendor object associated with the name selected by user
+		Vendor v = vendorService.findOne(regenInvoice.getVendorId());
+		
+		//Putting all projects associated with the vendor into a list
+		List<Project> projects = projectService.findByVendor(v);
+
+		//Cycling through projects list and appending all of the associated ProjectTimesheets into one list
+		//(within pay period)
+		List<ProjectTimesheet> projectTimesheets = new ArrayList<ProjectTimesheet>();
+		for (Project project : projects)
+			projectTimesheets.addAll(projTimeService.timesheetsForInvoice(project.getProjectId(), regenInvoice.getPeriodStart()));
+
+		//Creating new invoice
+		regenInvoice = new Invoice(projectTimesheets);
+		invoiceService.edit(regenInvoice);
+		
+		return "redirect:/invoice/view/" + invoiceId;
 	}
 	
 	//VIEWING ONE INVOICE
@@ -113,4 +134,5 @@ public class InvoiceController {
 		
 		return "invoice/viewI";
 	}
+
 }
